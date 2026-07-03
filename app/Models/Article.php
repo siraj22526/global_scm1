65ff8e9a6097ed945238f3ede631b97c7a45118c
sabrisiraj <shabrishiraj@gmail.com>
2026-07-04 00:33:12 +0700

    remove unused user relationship method from Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,8 +8,5 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 #[Fillable(['user_id', 'title', 'slug', 'body', 'status'])]
 class Article extends Model
 {
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+   
 }
