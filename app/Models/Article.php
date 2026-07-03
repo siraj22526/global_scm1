d2a2358217974bef93080a1919828c7a8e5e30df
sabrisiraj <shabrishiraj@gmail.com>
2026-07-04 00:34:04 +0700

    add user relationship method to Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,5 +8,8 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 #[Fillable(['user_id', 'title', 'slug', 'body', 'status'])]
 class Article extends Model
 {
-   
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
